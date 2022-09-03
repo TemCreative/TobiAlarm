f67fb554b191b720430410c316c29f7d6b7d5ae7
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-09-03 17:35:46 +0100

    i added myanswr.style.display

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-
+myanswr.style.display= "none"
+get.style.display="none"
+ function shwcrntTime(){
+     var myDate= new Date();
+     var myHour= myDate.getHours(); 
+     var myMinutes= myDate.getMinutes();
+     var mySeconds= myDate.getSeconds();
+     var ap= (myDate.getHours()) < 12 ? 'AM' : 'PM';
+     gettym.innerHTML= myHour + " : " + myMinutes + " : " + mySeconds + " " + ap;
+         
+     
+     if (hr.value == myHour && mn.value == myMinutes && sc.value == mySeconds) {
+         // alert("Hi")
+         snd.play()
+      }
+    
+ }
  var curntTime = setInterval(shwcrntTime,1000);
  function alarmSet(){
      var myDate= new Date();
