14c5525f2112ab5a34191184927b68b4e2baf655
TemCreative <109309894+TemCreative@users.noreply.github.com>
2022-09-03 17:34:54 +0100

    i added var curntTime

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ get.style.display="none"
       }
     
  }
+ var curntTime = setInterval(shwcrntTime,1000);
+ function alarmSet(){
+     var myDate= new Date();
+     var myHour= myDate.getHours(); 
+     var myMinutes= myDate.getMinutes();
+     var mySeconds= myDate.getSeconds();
+     if (hr.value == myHour && mn.value == myMinutes && sc.value == mySeconds) {
+         // alert("Hi")
+         snd.play();
+      }
+ }
